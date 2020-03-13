e826b0a9cd81e174a9a6394657c0bdb2d5361275
paula113 <paulapierina96@gmail.com>
2020-03-12 22:46:43 -0500

    practica

--- src/data.js
+++ src/data.js
@@ -1,5 +1,10 @@
 // estas funciones son de ejemplo
-
 // export const example = () => 'example';
-
 // export const anotherExample = () => 'OMG';
+// simport data from './data/atletas/atletas.js';
+
+// const olympicsData = data;
+// export const athletesList = () => {
+//   let addDataList = document.createElement('td');
+//   addDataList.textContent = olympicsData['name'];
+// };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-// import { example } from './data.js';
+// import athletesList  from './data.js';
 import data from './data/atletas/atletas.js';
 
+// const miraMasDeAtletas = document.getElementsById('miraMasDeAtletas');
+
+// miraMasDeAtletas.addEventListener('click', () => {
+// //   const miraMasDeAtleta = document.getElementById('miraMasDeAtletas');
+//   miraMasDeAtletas.style.visibility = 'visible';
+// });
+
+
 const olympic = data;
-document.getElementsByTagName('td').innerHTML = olympic.atletas[0].name;
-console.log(olympic.atletas[0].name);
+document.getElementsByTagName('td')[0].innerHTML = olympic.atletas[0].name;
+document.getElementsByTagName('td')[1].innerHTML = olympic.atletas[1].name;
+// document.getElementsByName('td').innerHTML=
+// console.log(olympic.atletas[0]);
+/*
+document.body.table.td.onload = addElement;
+function addElement () {
+  let newTd = document.createElement('td');
+  let newContent = document.createElement(atletas.name[3]);
+  newTd.appendChild(newContent);
+  let currentTd = document.getElementsByName('td');
+  document.body.table.insertBefore(newTd, currentTd);
+}
+*/
+const newTd = document.createElement('td');
+const newContent = document.createTextNode('aloha');
+const tableContent = document.getElementsByTagName('tbody')[0];
+tableContent.appendChild(newTd);
+newTd.innerHTML = newContent;
