b2f1a1626f3285e12c26be45c70b0458a27c94e7
paula113 <paulapierina96@gmail.com>
2020-03-15 12:12:12 -0500

    atletas table

--- src/data.js
+++ src/data.js
@@ -1,10 +1,7 @@
 // estas funciones son de ejemplo
 // export const example = () => 'example';
 // export const anotherExample = () => 'OMG';
-// simport data from './data/atletas/atletas.js';
+import data from './data/atletas/atletas.js';
 
-// const olympicsData = data;
-// export const athletesList = () => {
-//   let addDataList = document.createElement('td');
-//   addDataList.textContent = olympicsData['name'];
-// };
+
+export default data.js;
--- src/main.js
+++ src/main.js
@@ -1,49 +1,39 @@
-// import athletesList  from './data.js';
+// import { example } from './data.js';
 import data from './data/atletas/atletas.js';
-
-// const miraMasDeAtletas = document.getElementsById('miraMasDeAtletas');
-
-// miraMasDeAtletas.addEventListener('click', () => {
-// //   const miraMasDeAtleta = document.getElementById('miraMasDeAtletas');
-//   miraMasDeAtletas.style.visibility = 'visible';
-// });
+// import atletasNames from './data.js';
+// import markupTable from './data.js';
 
 
-const olympic = data;// ACCSESO AL OBJETO PROPIEDAD Y VAVOR (FUNCIONO)
-document.getElementsByTagName('td')[0].innerHTML = olympic.atletas[0].name;
-document.getElementsByTagName('td')[1].innerHTML = olympic.atletas[1].name;
-// document.getElementsByName('td').innerHTML=
-// console.log(olympic.atletas[0]);
+// data.js
+const atletasNames = names => `<tr><td>${names.name} <span class="tableLeft">${names.sport}</span></td></tr>`;
+const createOption = () => ` <option value="default">default</option> 
+`;
 
-/*
-INTENTANDO CREAR ELEMNTO PARA INSERTAR NOMBRE
-document.body.table.td.onload = addElement;
-function addElement () {
-  let newTd = document.createElement('td');
-  let newContent = document.createElement(olympic.name[3]);
-  newTd.appendChild(newContent);
-  let currentTd = document.getElementsByName('td');
-  document.body.table.insertBefore(newTd, currentTd);
-}
-
-*/
+// dropDown list con las olimpiadas
+const dropDownList = `
+  <div class = counterAndList>
+  <select class="olympicsDropdownList">
+    ${createOption}
+  </select>
+  <p class="counter">total de atletas ${data.atletas.length}</p>
+  </div>
+`;
 
 
-// Cree u elemto td en html y lo appende como ultimo child de tbody
-const newTd = document.createElement('td');
-const newContent = document.createTextNode('aloha');
-const tableContent = document.getElementsByTagName('tbody')[0];
-tableContent.appendChild(newTd);
-newTd.appendChild(newContent);
-// newTd.innerHTML = newContent;
-// olympic.forEach(addElement);
+// tabla de atletas nombres y deportes
+const markupTable = `
+<h1 class="title">ATLETAS OLIMPICOS</h1>
+${dropDownList}
+<table class="tableContent">
+    <thead>
+      <th>Nombre <span class="tableTitleLeft">Deporte</span></th>
+    </thead>
+    <tbody>
+    ${data.atletas.map(atletasNames).join('')}
+    </body>    
+  </table>
+`;
 
-/*
-const addElement = (ele => {
-  console.log(ele)
-  numCallbacksRun++
-});
-olympic.forEach(addElement);
-console.log('numCallBacksRun', numCallbacksRun)
-*/
-// olympic.foreach(ele => console.log(ele));
+// DOM
+document.getElementById('byName').innerHTML = markupTable;
+document.getElementsByClassName('title').innerHTML = dropDownList;
