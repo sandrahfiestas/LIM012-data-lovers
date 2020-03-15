cc764f3774eddf77e3d0d905ba50d194d2f59c2f
sandrahfiestas <shfiestas@gmail.com>
2020-03-15 10:57:50 -0500

    estructura primera historia,html,css y main.js muestra la lista de atletas de todas las olimpiadas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/atletas/atletas.js';
+
+
+const datAtletas = document.getElementById('atletasData');
+const olympic = data;
+
+const allAtletas = (olympic) => {
+  let info = '';
+  olympic.atletas.forEach((obj) => {
+    info += `
+        <div>
+             <p>${obj.name}</p>
+        </div>
+        `;
+  });
+  return info;
+};
+const btnMostrar = document.getElementById('btnMostrar');
+btnMostrar.addEventListener('click', () => {
+  datAtletas.innerHTML = allAtletas(olympic);
+});
 
 console.log(example, data);
